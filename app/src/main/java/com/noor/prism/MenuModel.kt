e49813d5e54c48b4f4c7de54e1fd6e7557a1cdeb
AmtunNoor <prideofmariam+github@gmail.com>
2026-06-08 20:01:03 +0300

    Update MenuModel.kt

--- app/src/main/java/com/noor/prism/MenuModel.kt
+++ app/src/main/java/com/noor/prism/MenuModel.kt
@@ -1,1 +1,7 @@
+package com.noor.prism
+import com.google.gson.annotations.SerializedName
 
+data class MenuModel(
+    @SerializedName("title") val title: String,
+    @SerializedName("url") val url: String
+)

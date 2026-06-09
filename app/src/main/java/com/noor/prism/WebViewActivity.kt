37254c481faea67eb98f8976cf3eae0db457e031
AmtunNoor <prideofmariam+github@gmail.com>
2026-06-10 00:11:30 +0300

    Update WebViewActivity.kt

--- app/src/main/java/com/noor/prism/WebViewActivity.kt
+++ app/src/main/java/com/noor/prism/WebViewActivity.kt
@@ -1,62 +1,46 @@
 package com.noor.prism
 
-import com.noorprism.app.R
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
-import android.webkit.WebChromeClient
+import android.view.KeyEvent
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-
 
 class WebViewActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
-    private lateinit var progressBar: ProgressBar
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_webview)
+        setContentView(R.layout.activity_web_view)
 
         webView = findViewById(R.id.webView)
-        progressBar = findViewById(R.id.webViewProgress)
-
-        window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
-
-        val targetUrl = intent.getStringExtra("TARGET_URL") ?: "https://google.com"
-
-        val settings = webView.settings
-        settings.javaScriptEnabled = true
-        settings.domStorageEnabled = true
-        settings.loadWithOverviewMode = true
-        settings.useWideViewPort = true
-        settings.mediaPlaybackRequiresUserGesture = false
-        settings.setSupportZoom(false)
+        
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.useWideViewPort = true
+        webSettings.loadWithOverviewMode = true
 
         webView.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
-                progressBar.visibility = View.GONE
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                url?.let { view?.loadUrl(it) }
+                return true
             }
         }
-        webView.webChromeClient = WebChromeClient()
-        webView.loadUrl(targetUrl)
+
+        val url = intent.getStringExtra("URL") ?: "https://google.com"
+        webView.loadUrl(url)
     }
 
-    override fun onBackPressed() {
-        if (webView.canGoBack()) {
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
             webView.goBack()
-        } else {
-            super.onBackPressed()
+            return true
         }
+        return super.onKeyDown(keyCode, event)
     }
 }

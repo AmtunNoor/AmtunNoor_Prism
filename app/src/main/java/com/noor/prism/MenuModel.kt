1952e8fe8fe026b96e709e50aa8a1b0e39f4dc40
AmtunNoor <prideofmariam+github@gmail.com>
2026-07-11 22:27:32 +0300

    Delete app/src/main/java/com/noor/prism/MenuModel.kt

--- app/src/main/java/com/noor/prism/MenuModel.kt
+++
@@ -1,7 +0,0 @@
-package com.noor.prism
-import com.google.gson.annotations.SerializedName
-
-data class MenuModel(
-    @SerializedName("title") val title: String,
-    @SerializedName("url") val url: String
-)

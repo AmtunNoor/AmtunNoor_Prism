692336bac2a1da4064cd2082e2228d5b6caa0424
AmtunNoor <prideofmariam+github@gmail.com>
2026-07-12 17:19:58 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,4 @@
-android.useAndroidX=true
-android.enableJetifier=true
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+}

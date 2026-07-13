2e1a84396b0dcec08cac97fa7057c4289bcf3473
AmtunNoor <prideofmariam+github@gmail.com>
2026-07-13 23:56:16 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.noor.prism"
         minSdk = 21
         targetSdk = 35
-        versionCode = 17
-        versionName = "3.0.0-rc2"
+        versionCode = 18
+        versionName = "3.0.0-rc3"
     }
 
     buildFeatures {
